e3a05c7b295f91fa3241a37e83552599941272e3
Sammy Teahan <sam.teahan@aggiemail.usu.edu>
2015-03-06 10:11:22 -0700

    old working prototype

--- src/app/dashboard/dashboard.js
+++ src/app/dashboard/dashboard.js
@@ -10,15 +10,7 @@
         'main': {
           controller: 'DashboardCtrl',
           controllerAs: 'vm',
-          templateUrl: 'dashboard/dashboard.tpl.html',
-          resolve: {
-            userData: ['UserFactory', function(UserFactory) {
-              return UserFactory.getUser();
-            }],
-            projectData: ['ProjectFactory', function(ProjectFactory) {
-              return ProjectFactory.getProjects();
-            }]
-          }
+          templateUrl: 'dashboard/dashboard.tpl.html'
         }
       }
     });
@@ -31,15 +23,11 @@
     '$state', 
     'ProjectFactory',
     'UserFactory',
-    'userData',
-    'projectData',
   function (
     $scope, 
     $state,
     ProjectFactory,
-    UserFactory,
-    userData,
-    projectData
+    UserFactory
   ) {
     var vm = this;
     vm.title = 'Dashboard';
@@ -49,25 +37,26 @@
     vm.finishedStories = 1;
     vm.currentStories = 1;
     vm.bugs = 1;
-    // vm.projectNums = [1, 1, 1, 1];
-    vm.projectNums = [Math.floor(Math.random()*100), Math.floor(Math.random()*100), Math.floor(Math.random()*100), Math.floor(Math.random()*100)];
+    vm.projectNums = [1, 1, 1, 1];
 
-    vm.user = userData.data;
+    ProjectFactory.getProjects().then(onProjectsSuccess, onFailure);
 
-    // ProjectFactory.getProjects().then(onProjectsSuccess, onFailure);
+    vm.getUser = function() {
+      UserFactory.getUser().then(onSuccess, onFailure);
+    };
 
-    vm.projects = projectData.data.projects;
-    vm.title = vm.projects[vm.current].project_name;
-    vm.projectIds = vm.projects.map(function (project) {
-      return project.project_id;
-    });
-    // function onSuccess(response) {
-    //   vm.user = response.data;
-    // }
+    function onSuccess(response) {
+      vm.user = response.data;
+    }
 
     // get projects and store all id's in new array
-    function onProjectsSuccess(response) { /*** take this func out ***/
-      vm.getProjectData(vm.projectIDs[0]); /*** re-do these calls to get specific project story info ***/
+    function onProjectsSuccess(response) {
+      vm.projects = response.data.projects;
+      vm.title = response.data.projects[vm.current].project_name;
+      vm.projectIDs = vm.projects.map(function (project) {
+        return project.project_id;
+      });
+      vm.getProjectData(vm.projectIDs[0]);
       vm.getProjectStories(vm.projectIDs[0]);
     }
 
@@ -80,7 +69,7 @@
       ProjectFactory.getProject(id).then(projectSuccess, onFailure);
     };
 
-    vm.getProjectStories = function(id) { /*** take this out and replace with api calls with parameters ***/
+    vm.getProjectStories = function(id) {
       ProjectFactory.getProjectStories(id).then(storiesSuccess, onFailure);
     };
 
@@ -106,6 +95,15 @@
       vm.projectNums = [vm.bugs.length, vm.unstartedStories.length, vm.newBugs.length, vm.stories.length];
     }
 
+    vm.changeData = function() {
+      vm.projectNums = [
+        Math.floor(Math.random() * 50), 
+        Math.floor(Math.random() * 50), 
+        Math.floor(Math.random() * 50), 
+        Math.floor(Math.random() * 50)
+      ];
+    };
+
     vm.getNext = function() {
       ProjectFactory.getProjects().then(function success(response) {
         vm.projectCount = response.data.projects.length;
@@ -116,7 +114,8 @@
       }, onFailure);
       if(vm.current % vm.projectCount === 0) {
         vm.current = 0;
-        ProjectFactory.getProjectStories(vm.projectIDs[vm.current]).then(function success(response) {
+        console.log(vm.current);
+        ProjectFactory.getProjectStories(vm.projectIDs[++vm.current]).then(function success(response) {
           vm.stories = response.data;
 
           vm.unstartedStories = vm.stories.filter(function(story) {
@@ -140,6 +139,8 @@
       else {
         ProjectFactory.getProjectStories(vm.projectIDs[++vm.current]).then(function success(response) {
           vm.stories = response.data;
+          console.log(vm.current);
+
 
           vm.unstartedStories = vm.stories.filter(function(story) {
             return story.estimate != null && story.current_state === 'planned';
@@ -161,22 +162,8 @@
       }
     };
 
-    vm.changeData = function() {
-      vm.projectNums = [
-        Math.floor(Math.random() * 50), 
-        Math.floor(Math.random() * 50), 
-        Math.floor(Math.random() * 50), 
-        Math.floor(Math.random() * 50)
-      ];
-    };
-
-    // vm.printNext() = function() {
-    //   vm.current++;
-    //   if(vm.current % vm.projects.length === 0) {
-    //     vm.current = 0;
-    //     console.log(vm.projects[vm.current]);
-    //   }
-    // }
+    // Init user to get projects
+    vm.getUser();
 
   }]);
 }());
--- src/common/donutChart/donutChart.js
+++ src/common/donutChart/donutChart.js
@@ -13,176 +13,60 @@
       templateUrl: 'donutChart/donutChart.tpl.html',
       link: function (scope, iElement, iAttrs) {
 
+        var color = d3.scale.category10();
         var element = iElement[0];
-        var svg = d3.select(element)
-          .append("svg")
-          .append("g");
-
-        svg.append("g")
-          .attr("class", "slices");
-        svg.append("g")
-          .attr("class", "labels");
-        svg.append("g")
-          .attr("class", "lines");
-
-        var width = 450,
-            height = 450,
-            radius = Math.min(width, height) / 2;
-
-        var data = [{"label":"one", "value":30}, 
-            {"label":"two", "value":50}, 
-            {"label":"three", "value":30}];
-
-        var pie = d3.layout.pie()
-          .sort(null)
-          .value(function(d) {
-            return d.value;
-          });
-
+        var width = 750;
+        var height = 750;
+        var radius = Math.min(width, height);
+        var pie = d3.layout.pie().sort(null);
         var arc = d3.svg.arc()
-          .outerRadius(radius * 0.8)
-          .innerRadius(radius * 0.5);
+          .outerRadius(radius / 2 * 0.9)
+          .innerRadius(radius / 2 * 0.5);
+        var svg = d3.select(element).append('svg')
+          .attr({
+            width: width,
+            height: height
+          })
+          .append('g')
+          .attr('transform', 'translate(' + width / 2 + ',' + height / 2 + ')');
 
-        var outerArc = d3.svg.arc()
-          .innerRadius(radius * 0.9)
-          .outerRadius(radius * 0.9);
+        // lets try to append labels
+        svg.append('g')
+          .attr('class', 'labels');
 
-        svg.attr("transform", "translate(" + width / 2 + "," + height / 2 + ")");
-
-        // do i need dis??? 
-        var key = function(d){ return d.data.label; };
-
-        var color = d3.scale.ordinal()
-          .domain(["Unstarted Stories", "Currently working on", "Open Bugs", "Stories ready for review"])
-          .range(["#2ecc71", "#e67e22", "#e74c3c", "#3498db"]);
-
-        // Put scope.data values into an array and put them in the labels map function to map to the correct ordinal numbers
-        function randomData (){
-          var labels = color.domain();
-          var counter = 0;
-          var values = scope.data.map(function(value) {
-            return value;
-          });
-          return labels.map(function(label){
-            return { label: label, value: Math.random() };
-          });
+        function arcTween(a) {
+          var i = d3.interpolate(this._current, a);
+          this._current = i(0);
+          return function(t) {
+            return arc(i(t));
+          };
         }
-
-        change(randomData());
-
-        d3.select(".randomize")
-          .on("click", function(){
-            change(randomData());
-          });
-
-        function change(data) {
-
-          /**
-          * PIE SLICES
-          */
-          var slice = svg.select(".slices").selectAll("path.slice")
-            .data(pie(data), key);
-
-          slice.enter()
-            .insert("path")
-            .style("fill", function(d) { return color(d.data.label); })
-            .attr("class", "slice");
-
-          slice   
-            .transition().duration(1000)
-            .attrTween("d", function(d) {
-              this._current = this._current || d;
-              var interpolate = d3.interpolate(this._current, d);
-              this._current = interpolate(0);
-              return function(t) {
-                return arc(interpolate(t));
-              };
-            });
-
-          slice.exit()
-            .remove();
-
-          /**
-          * TEXT LABELS
-          */
-          var text = svg.select(".labels").selectAll("text")
-            .data(pie(data), key);
-
-          text.enter()
-            .append("text")
-            .attr("dy", ".35em")
-            .style('fill', '#ffffff')
-            .attr('font-size', '28px')
-            .text(function(d) {
-              return d.data.label;
-            });
-          
-          function midAngle(d){
-            return d.startAngle + (d.endAngle - d.startAngle)/2;
-          }
-
-          text.transition().duration(1000)
-            .attrTween("transform", function(d) {
-              this._current = this._current || d;
-              var interpolate = d3.interpolate(this._current, d);
-              this._current = interpolate(0);
-              return function(t) {
-                var d2 = interpolate(t);
-                var pos = outerArc.centroid(d2);
-                pos[0] = radius * (midAngle(d2) < Math.PI ? 1 : -1);
-                return "translate("+ pos +")";
-              };
+        
+         // add the paths for each arc slice
+        var arcs = svg.selectAll('path').data(pie(scope.data))
+          .enter().append('path')
+            .style('stroke', 'white')
+            .attr('fill', function(d, i) { 
+              return color(i); 
             })
-            .styleTween("text-anchor", function(d){
-              this._current = this._current || d;
-              var interpolate = d3.interpolate(this._current, d);
-              this._current = interpolate(0);
-              return function(t) {
-                var d2 = interpolate(t);
-                return midAngle(d2) < Math.PI ? "start":"end";
-              };
+            // store the initial angles
+            .each(function(d) { 
+              this._current = d;
+              return this._current; 
             });
 
-          text.exit()
-            .remove();
+        var labels = svg.select('.labels').selectAll('path.slice')
+          .data(pie(scope.data));
+        
+        // update the arcs
+        scope.$watch('data', function(data){
+          arcs.data(pie(data)).transition().attrTween('d', arcTween);
+        });
 
-          /**
-          * SLICE TO TEXT POLYLINES
-          */
-          var polyline = svg.select(".lines").selectAll("polyline")
-            .data(pie(data), key);
-
-          polyline.enter()
-            .append("polyline");
-
-          polyline.transition().duration(1000)
-            .attrTween("points", function(d){
-              this._current = this._current || d;
-              var interpolate = d3.interpolate(this._current, d);
-              this._current = interpolate(0);
-              return function(t) {
-                var d2 = interpolate(t);
-                var pos = outerArc.centroid(d2);
-                pos[0] = radius * 0.95 * (midAngle(d2) < Math.PI ? 1 : -1);
-                return [arc.centroid(d2), outerArc.centroid(d2), pos];
-              };      
-            });
-          
-          polyline.exit()
-            .remove();
-
-          // // ---- NEED TO UPDATE ON SCOPE CHANGE ---- //
-          // scope.$watch('data', function(data){
-          //   slice.data(pie(data), key).transition().attrTween('d', change(randomData()));
-          // });
-
-          // our data changed! update the arc paths
-          // scope.$watch('data', function(data) {
-          //   slice.data(pie(data), key).transition().attrTween('d', change(randomData()));
-          // });
-
-        } // END OF CHANGE FUNCTION
-
+        // our data changed! update the arc <path>s
+        scope.$watch('data', function(data) {
+          arcs.data(pie(data)).attr('d', arc);
+        });
 
       }
     };
